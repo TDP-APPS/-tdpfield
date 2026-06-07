9b98df545db077fb9d6de8e108ceb26e777283c1
TDP <mahmoudtdp1@gmail.com>
2026-06-07 11:57:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,8 @@
-const CACHE = 'tdp-field-v1';
+const CACHE = 'tdp-field-v2';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon.svg',
   '/icon-192.png',
   '/icon-512.png'
 ];
@@ -27,15 +26,12 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(res => {
-        if (res && res.status === 200 && res.type === 'basic') {
-          const clone = res.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        }
-        return res;
-      }).catch(() => caches.match('/index.html'));
-    })
+    fetch(e.request).then(res => {
+      if (res && res.status === 200 && res.type === 'basic') {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match(e.request).then(cached => cached || caches.match('/index.html')))
   );
 });
